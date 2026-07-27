b0a356508c7969c9cfd03d32ce3f8fb5e79ade0b
Ashwini-Bhadane-1 <154913171+Ashwini-Bhadane-1@users.noreply.github.com>
2026-07-27 00:00:24 -0400

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,13 @@
+import Java.io.*;
+public class Sample
+{
+	public static void main(String args[]);
+	{
+		int i=0,n=5,count=0;
+		for(i=0;i<n;i++)
+		{
+			count++;
+		}
+		System.out.println("count="+count);
+	}
+}
